efd615648671249b921d03b6d027d0d7be69b1de
himanxhu1233 <dhandolehimanshu@gmail.com>
2026-05-31 14:31:24 +0530

    added dummy data in backend

--- backend/src/main/java/com/teamarc/proxima/repository/StudentRepository.java
+++ backend/src/main/java/com/teamarc/proxima/repository/StudentRepository.java
@@ -1,10 +1,13 @@
 package com.teamarc.proxima.repository;
 
 import com.teamarc.proxima.entity.Student;
+import com.teamarc.proxima.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
+    Optional<Student> findByUser(User user);
 }
